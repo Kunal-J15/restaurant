03ed07a4171a3b647298b5c42e848c4f7eef651d
KJ <subodhsjagtap19@gmail.com>
2023-04-29 17:57:24 +0530

    summery div

--- src/App.js
+++ src/App.js
@@ -1,12 +1,17 @@
 import Headers from "./Components/layout/Header";
-
+import Summery from "./Components/layout/Summary";
 function App() {
   
   return (
     <div>
-    <Headers></Headers>    
+    <Headers></Headers>  
+    <main>
+    <Summery></Summery>  
+    </main>
+
     </div>
   );
 }
 
 export default App;
+
---
+++ src/Components/layout/Summary.js
@@ -0,0 +1,13 @@
+import React,{  } from "react";
+
+import "./Summary.css"
+const summery = (props)=>{
+
+    return (<div className="summary">
+        <h1>Delicious Food, Deliver To You</h1>
+        <p>Choose your food from our broad section of available meals and enjoy delicious lunch or dinner at your home.</p>
+        <p>All our meals are cooked with high-quality ingredients, just in time and experienced chefs.</p>
+    </div>
+        )
+}
+export default summery;
